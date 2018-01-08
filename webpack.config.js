0d5d9a9ebb00ce36959448eeaaea5f95bbbf28b9
Arman Yessymkanov <arman.yessymkanov@gmail.com>
2018-01-08 21:06:31 +0600

    initial commit, set up environment

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/public',
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        use: 'babel-loader',
+        exclude: /(node_modules)/
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin('./index.html')
+  ],
+  devServer: {
+    port: 8000,
+    compress: true,
+    inline: true,
+  }
+}
+
+module.exports = config;
